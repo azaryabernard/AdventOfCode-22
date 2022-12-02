4b1d51a13ff58f452c2a34cd635cbc4ab3b7fbb3
Azaya Bernard Haposan <azarya.haposan@tum.de>
2022-12-02 09:21:46 +0100

    update of day 2 challenge - simplification

--- Day 2 - Rock Paper Scissor/src/Main.hs
+++ Day 2 - Rock Paper Scissor/src/Main.hs
@@ -1,40 +1,29 @@
 module Main (main) where
 
--- A = X: Rock, B = Y: Paper, C = Z: Scissors
+-- A: Rock, B: Paper, C: Scissors | A beats C, B beats A, C beats B
+-- m: (0 -> X: Rock, Y: Paper, Z: Scissors), (1 -> X: Lose, Y: Draw, Z: Win)
 -- Type  = (Rock) 1, (Paper) 2, (Scissors) 3
 -- Eval  = (Win) 6, (Draw) 3, (Lose) 0
 -- Score = Type + Eval
-eval :: [Char] -> Int
-eval xs = case (head xs, last xs) of 
-  ('A','X') -> 4
-  ('A','Y') -> 8
-  ('A','Z') -> 3
-  ('B','X') -> 1
-  ('B','Y') -> 5
-  ('B','Z') -> 9
-  ('C','X') -> 7
-  ('C','Y') -> 2
-  ('C','Z') -> 6
-  (_, _)    -> 0
-
--- X: Lose, Y: Draw, Z: Win
-eval' :: [Char] -> Int
-eval' xs = case (head xs, last xs) of 
-  ('A','X') -> 3
-  ('A','Y') -> 4
-  ('A','Z') -> 8
-  ('B','X') -> 1
-  ('B','Y') -> 5
-  ('B','Z') -> 9
-  ('C','X') -> 2
-  ('C','Y') -> 6
-  ('C','Z') -> 7
+eval :: Int -> [Char] -> Int
+eval m xs 
+ | m `notElem` [0, 1] = 0
+ | otherwise = case (head xs, last xs) of 
+  ('A','X') -> [4, 3]!!m
+  ('A','Y') -> [8, 4]!!m
+  ('A','Z') -> [3, 8]!!m
+  ('B','X') -> [1, 1]!!m
+  ('B','Y') -> [5, 5]!!m
+  ('B','Z') -> [9, 9]!!m
+  ('C','X') -> [7, 2]!!m
+  ('C','Y') -> [2, 6]!!m
+  ('C','Z') -> [6, 7]!!m
   (_, _)    -> 0
 
 -- main, assuming input is valid
 main :: IO ()
 main = do
-  pairs eval >>= putStrLn . (++) "Score from strategy: "
-  pairs eval' >>= putStrLn . (++) "Score from top secret strategy: "
+  pairs (eval 0) >>= putStrLn . (++) "Score from strategy: "
+  pairs (eval 1) >>= putStrLn . (++) "Score from top secret strategy: "
   where pairs f = do
          show . sum . map f . lines <$> readFile "src/input.data"
