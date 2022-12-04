8003a45591d3e75ae27a2c07f0e63d3381b71470
Azaya Bernard Haposan <azarya.haposan@tum.de>
2022-12-04 12:48:33 +0100

    rename some stuff in Day04

--- Day 4 - Camp Cleanup/Day04.hs
+++ Day 4 - Camp Cleanup/Day04.hs
@@ -7,15 +7,15 @@ filterContain :: [[[Int]]] -> [[[Int]]]
 filterContain = filter $ \xs -> head xs `isInfixOf` last xs || last xs `isInfixOf` head xs
 
 filterOverlap :: [[[Int]]] -> [[[Int]]]
-filterOverlap = filter $ \xs -> not . null $ head xs `intersect` last xs
+filterOverlap = filter $ \xs -> not.null $ head xs `intersect` last xs
 
 -- main, assuming input is valid. Yes, I love one liners. :)
 day04 :: IO ()
 day04 = do 
   input <- map (splitOn ",") . lines <$> readFile "input.data"
-  let parsed = map.map $ \xs -> let ys = splitOn "-" xs in [read.head $ ys .. read.last $ ys]
-  -- parsed :: [[[Int]]] = [[[14..38], [14..14]], [[2..10], [3..55]], ...]
+  let parse  = map.map $ \xs -> let ys = splitOn "-" xs in [read.head $ ys .. read.last $ ys]
+  -- parse :: [[[Char]]] -> [[[Int]]] = [[[14..38], [14..14]], [[2..10], [3..55]], ...]
   putStrLn $ (++) "Total assignment pairs of one range fully contains the other: " 
-           $ show . length . filterContain . parsed $ input
+           $ show . length . filterContain . parse $ input
   putStrLn $ (++) "Total assignment pairs that overlap at all: " 
-           $ show . length . filterOverlap . parsed $ input
+           $ show . length . filterOverlap . parse $ input
