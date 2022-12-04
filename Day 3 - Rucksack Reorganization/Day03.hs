a5f0900ee46ad086933bcde331f9c80ba26ff5e4
Azaya Bernard Haposan <azarya.haposan@tum.de>
2022-12-04 20:16:22 +0100

    refactor code for readability + naming

--- Day 3 - Rucksack Reorganization/Day03.hs
+++ Day 3 - Rucksack Reorganization/Day03.hs
@@ -8,15 +8,15 @@ import Data.Char (ord, isUpper, isAlpha)
 eval :: Char -> Int
 eval c | isAlpha c = ord c - if isUpper c then 38 else 96 | otherwise = 0
 
-prio :: [String] -> Int
-prio = sum . map (eval . head . \xs -> let half = length xs`div`2 in intersect (take half xs) (drop half xs))
+priority :: [String] -> Int
+priority = sum . map (eval . head . \xs -> let half = length xs`div`2 in intersect (take half xs) (drop half xs))
 
 badge :: [String] -> Int
-badge input = sum $ eval . head . foldr1 intersect <$> chunksOf 3 input
+badge = sum . map (eval . head . foldr1 intersect) . chunksOf 3
 
 -- main, assuming input is valid
 run :: IO ()
 run = do
   input <- lines <$> readFile "input.data"
-  putStrLn $ "Sum of the priorities of all item types: " ++ show (prio input)
+  putStrLn $ "Sum of the priorities of all item types: " ++ show (priority input)
   putStrLn $ "Sum of the priorities of the team badges: " ++ show (badge input)
