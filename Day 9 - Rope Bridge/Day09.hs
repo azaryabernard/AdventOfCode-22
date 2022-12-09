0598e17469f02a02fe3923cdd200c5c9951a422c
Azaya Bernard Haposan <azarya.haposan@tum.de>
2022-12-09 19:47:28 +0100

    update Day 9 solution - sugar update

--- Day 9 - Rope Bridge/Day09.hs
+++ Day 9 - Rope Bridge/Day09.hs
@@ -1,14 +1,12 @@
 module Day09 (run) where
 
 import Data.Bifunctor(bimap)
-import Data.Set as Set (fromList, toList)
+import Data.List (nub)
 
--- syntactic sugar
 type Position = (Int, Int)
 type Step = Int
 data Direction = U | D | L | R | UL | UR | DL | DR deriving (Show, Eq, Enum, Read)
 
--- move the index in a direction by 1
 moveIndex :: Direction -> Position -> Position
 moveIndex direction (x,y) = case direction of
   U -> (x, y-1);  UL -> (x-1, y-1)
@@ -16,65 +14,53 @@ moveIndex direction (x,y) = case direction of
   L -> (x-1, y);  DL -> (x-1, y+1)
   R -> (x+1, y);  DR -> (x+1, y+1)
 
--- find the direction according to the position of the previous node to the next node
-directionFinder :: Position -> Position -> Direction
-directionFinder (xh, yh) (xt, yt) 
-  | xt < xh && yt < yh  = DR
+findDirection :: Position -> Position -> Direction
+findDirection (xt, yt) (xh, yh) 
+  | xt < xh && yt < yh  = DR  
   | xt > xh && yt < yh = DL
-  | xt < xh && yt > yh =  UR
+  | xt < xh && yt > yh =  UR  
   | xt > xh && yt > yh = UL
-  | yt < yh = D
-  | yt > yh = U
-  | xt < xh = R
-  | xt > xh = L
-  | otherwise = error "directionFinder: undefined"
+  | yt < yh = D | yt > yh = U 
+  | xt < xh = R | xt > xh = L
+  | otherwise = error "findDirection: undefined"
 
--- is a node around another node?
 nodeAroundNode :: Position -> Position -> Bool 
-nodeAroundNode (xh, yh) (xt, yt) = or [(xt, yt) == (xh+i, yh+j) | i <- [-1, 0, 1], j <- [-1, 0, 1]]
+nodeAroundNode node (x, y) = or [node == (x+i, y+j) | i <- [-1, 0, 1], j <- [-1, 0, 1]]
 
 -- to let the previous node follow the next node
 followNext :: Position -> Position -> Position
-followNext nextPos prevPos
-  | nodeAroundNode nextPos prevPos = prevPos
-  | otherwise = moveIndex (directionFinder nextPos prevPos) prevPos
+followNext prevPos nextPos
+  | nodeAroundNode prevPos nextPos = prevPos
+  | otherwise = moveIndex (findDirection prevPos nextPos) prevPos
 
--- move the rope and all its tails 
+-- move the rope and all its tails, move head first, then the tails follow next
 moveNodes :: Direction -> Step ->  [Position] -> [[Position]]
 moveNodes _ 0 ps = [ps]
 moveNodes direction len ps = ps : moveNodes direction (len-1) ps''
-  where ps'' = scanl1 followNext ps' -- scan is fold that retains the process
+  where ps'' = scanl1 (flip followNext) ps' -- scan is fold that retains the process
         ps' = moveIndex direction (head ps) : tail ps
 
--- move the rope with all the steps in the directions
-moveRope :: [(Direction, Step)] -> [Position] -> [[Position]]
-moveRope [] ps = [ps]
-moveRope ((direction, len) : xs) ps = ys ++ moveRope xs ps'
+-- move the rope with all the steps in the directions, save the history
+moveRope ::  [Position] -> [(Direction, Step)] -> [[Position]]
+moveRope ps [] = [ps]
+moveRope ps ((direction, len) : xs) = ys ++ moveRope ps' xs
   where ps' = if null ys then ps else last ys
         ys = moveNodes direction len ps
 
--- distinct positions of the tail
 traceTail :: [[Position]] -> [Position]
-traceTail = Set.toList . Set.fromList . map last
+traceTail = nub . map last -- distinct positions of the tail
 
--- Init rope can be [Head, Tail] or [Head .. Tail]
-initRope :: Int -> [Position]
-initRope = flip replicate (0,0)
-
--- Extra: pretty print the grid for tail positions 😉
-prettyPrint :: [Position] -> IO ()
-prettyPrint ps = mapM_ putStrLn fillGrid
+prettyPrint :: [Position] -> IO () -- print the grid 😉
+prettyPrint ps =  putStrLn "" >> mapM_ putStrLn fillGrid
   where fillGrid = [[if (x,y) `elem` ps then 'X' else '.' | x <- [minX..maxX]] | y <- [minY..maxY]]
-        (minX, minY) = (minimum . map fst $ ps, minimum . map snd $ ps) 
+        (minX, minY) = (minimum . map fst $ ps, minimum . map snd $ ps)
         (maxX, maxY) = (maximum . map fst $ ps, maximum . map snd $ ps)
 
 -- main, assuming input is valid.
 run :: IO ()
 run = do 
   input <- map (bimap (\x->read x::Direction) (\x->read x::Step) . splitAt 1) . lines <$> readFile "input.data"
-  putStr "Visited Positions of the rope's tail: "
-  let part1 = traceTail . moveRope input $ initRope 2
-  print (length part1) >> prettyPrint part1
-  putStr "Visited Positions of the its extended tail: "
-  let part2 = traceTail . moveRope input $ initRope 10
-  print (length part2) >> prettyPrint part2
+  let part1 = traceTail $ moveRope (replicate 2 (0,0)) input
+  prettyPrint part1 >> putStrLn ("Visited Positions of the rope's tail: " ++ show (length part1))
+  let part2 = traceTail $ moveRope (replicate 10 (0,0)) input
+  prettyPrint part2 >> putStrLn ("Visited Positions of the rope and its extended tail: " ++ show (length part2))
